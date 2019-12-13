e74e4ca240fb686032332480d0f1f39992bbfabd
Anton Sozonenko <sozonenko.anton@gmail.com>
2019-12-13 13:40:32 +0200

    Fix

---
+++ stream_site/src/components/HeaderNav.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="absolute w-full bg-gray-900 md:bg-transparent md:flex md:justify-between md:items-center md:px-4 md:py-3">
+    <div class="flex items-center justify-between px-4 py-3 md:p-0">
+      <div>
+        <img class="h-8" :src="logo" alt="Logo">
+      </div>
+      <div class="md:hidden">
+        <button @click="isOpen = !isOpen" type="button" class="block text-gray-500 hover:text-white focus:text-white focus:outline-none">
+          <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+            <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
+            <path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+          </svg>
+        </button>
+      </div>
+    </div>
+    <nav :class="isOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4 md:flex md:p-0">
+      <router-link  to="/" class="block px-2 py-1 text-white rounded hover:bg-gray-800">Home</router-link>
+      <router-link to="/about" class="mt-1 block px-2 py-1 text-white rounded hover:bg-gray-800 md:mt-0 md:ml-2">About</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'HeaderNav',
+    data() {
+      return {
+        logo: require('../assets/logo.png'),
+        isOpen: false,
+      }
+    },
+  }
+</script>
--- stream_site/src/views/Home.vue
+++ stream_site/src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home bg-red-900 text-center">
-    <img class="mx-auto" alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home h-screen p-12 bg-black bg-center bg-cover bg-darken" :style="'background-image:url(' + dota2Bg + ');'">
+    <div class="w-full h-full p-6 overflow-y-auto">
+      <div class="flex flex-wrap">
+        <div class="w-1/3 p-1">
+          <div class="border rounded-sm bg-black">
+            <img src="https://via.placeholder.com/500x300" alt="">
+            <h3 class="px-4 mb-2 text-xl text-left text-green-500">Lorem ipsum dolor.</h3>
+            <small class="block px-4 mb-2 text-left text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, rem.</small>
+          </div>
+        </div>
+        <div class="w-1/3 p-1">
+          <div class="border rounded-sm bg-black">
+            <img src="https://via.placeholder.com/500x300" alt="">
+            <h3 class="px-4 mb-2 text-xl text-left text-green-500">Lorem ipsum dolor.</h3>
+            <small class="block px-4 mb-2 text-left text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, rem.</small>
+          </div>
+        </div>
+        <div class="w-1/3 p-1">
+          <div class="border rounded-sm bg-black">
+            <img src="https://via.placeholder.com/500x300" alt="">
+            <h3 class="px-4 mb-2 text-xl text-left text-green-500">Lorem ipsum dolor.</h3>
+            <small class="block px-4 mb-2 text-left text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, rem.</small>
+          </div>
+        </div>
+        <div class="w-1/3 p-1">
+          <div class="border rounded-sm bg-black">
+            <img src="https://via.placeholder.com/500x300" alt="">
+            <h3 class="px-4 mb-2 text-xl text-left text-green-500">Lorem ipsum dolor.</h3>
+            <small class="block px-4 mb-2 text-left text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, rem.</small>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
+  data() {
+    return {
+      dota2Bg: require('../assets/dota2_bg.jpg'),
+    }
+  },
   components: {
-    HelloWorld
+    //HelloWorld
   }
 }
 </script>
+
+
+<style scoped>
+  .bg-darken {
+    background-blend-mode: darken;
+    background-color: rgba(0, 0, 0, 0.78);
+  }
+</style>
